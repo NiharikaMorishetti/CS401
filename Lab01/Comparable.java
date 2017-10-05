eef833cc38f4f68a23182986e44125688fb8cf71
YC-S <yuanchenshi@gmail.com>
2017-10-05 12:19:09 -0500

    remove lab01

--- Lab01/Comparable.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public interface Comparable<Employee> {
-	public int compareTo(Employee employee1);
-	public String toString();
-	public int compare(Employee employee1, Employee employee2);
-	public boolean equals(test.Employee employee1);
-}
--- Lab01/Comparator.java
+++
@@ -1,6 +0,0 @@
-package test;
-
-public interface Comparator <Employee>{
-	int compare(Employee employee1, Employee employee2);
-	public boolean equals(test.Employee employee1);
-}
